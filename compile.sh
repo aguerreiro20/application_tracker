b43f83d17a1bd53f5a27b4c80d95160debe7b3fa
aguerreiro20 <guerreiroa28@gmail.com>
2025-02-11 22:47:42 +0000

    clean commit

--- compile.sh
+++
@@ -1,1 +0,0 @@
-g++ src/*.cpp -o prog -Wall -Wextra -Wsign-conversion -std=c++20
--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 #ifndef VERSION_H
 #define VERSION_H
-const char GIT_COMMIT_HASH[]  = "83d4340d236e2fdce7f02cca03fef272248faf61";
-const char GIT_COMMIT_MESSAGE[]  = "remotely updated readme file";
+const char GIT_COMMIT_HASH[]  = "99eb7aeaf061cb86bdf11a0b6b1fb4b99e0580cf";
+const char GIT_COMMIT_MESSAGE[]  = "git info in C++ code";
 #endif
